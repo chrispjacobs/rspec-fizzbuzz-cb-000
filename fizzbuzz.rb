a1e98e3d46b0f5cd0294d3704eda6342c989e390
Christopher Jacobs <chrisj.3222@gmail.com>
2018-10-08 21:56:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  number = number.to_f
   if number % 3 == 0 && number % 5 == 0
     return "FizzBuzz"
   elsif number % 5 == 0
